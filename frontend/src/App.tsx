2ab20fcb78dce75a2aba2dd035fc36c639697136
Nuriddin Abdullayev <nuriddinabdullayev0722@gmail.com>
2026-07-27 23:50:16 +0500

    added frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+
+      <button onClick={() => { setCount((count) => count + 1) }}>
+        Count is {count}
+      </button>
+    </div>
+  )
+}
+
+export default App
